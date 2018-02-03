8b58e2b67c8c9e67de52a01e2853f144b0dd30e1
Blaine Richardson <blainerichardson@Blaines-MacBook-Pro.local>
2018-02-03 10:41:42 -0600

    init

---
+++ app.js
@@ -0,0 +1,24 @@
+const { ObjectID } = require('mongodb');
+
+const queries = require('./database/queries');
+
+const mongoUrl = 'mongodb://localhost:27017';
+const dbName = 'TodoApp';
+
+let collectionName = 'Todos';
+
+let userObject = {
+    name: 'Blaine',
+    age: 26,
+    location: 'Des Moines, IA'
+}
+
+let queryConstraintCompleted = {
+    completed: false
+}
+
+let queryConstraintId = {
+    _id: new ObjectID('5a75d78d4a9f1b45e43637b8')
+}
+
+queries.countTodos(mongoUrl, dbName, collectionName);
---
+++ database/queries.js
@@ -0,0 +1,86 @@
+// const MongoClient = require('mongodb').MongoClient;
+
+const { MongoClient } = require('mongodb');
+
+
+module.exports = {
+    
+    newUser: (async (mongoUrl, dbName, collectionName, userObject) => {
+        let client;
+    
+        try {
+            client = await MongoClient.connect(mongoUrl);
+            console.log("Connected correctly to server");
+    
+            const db = client.db(dbName);
+    
+            let r = await db.collection(collectionName).insertOne(userObject);
+            
+            console.log(r.ops);
+            console.log(r.ops[0]._id.getTimestamp());
+        } catch (err) {
+            console.log('Unable to insert document.', err.stack)
+        }
+    
+        client.close();
+    }),
+
+    findTodos: (async (mongoUrl, dbName, collectionName) => {
+        let client;
+    
+        try {
+            client = await MongoClient.connect(mongoUrl);
+            console.log("Connected correctly to server");
+    
+            const db = client.db(dbName);
+    
+            let r = await db.collection(collectionName).find().toArray();
+            
+            console.log(r);
+        } catch (err) {
+            console.log('Unable to insert document.', err.stack)
+        }
+    
+        client.close();
+    }),
+
+    findSpecificTodos: (async (mongoUrl, dbName, collectionName, queryConstraint) => {
+        let client;
+    
+        try {
+            client = await MongoClient.connect(mongoUrl);
+            console.log("Connected correctly to server");
+    
+            const db = client.db(dbName);
+    
+            let r = await db.collection(collectionName).find(queryConstraint).toArray();
+            
+            console.log(r);
+        } catch (err) {
+            console.log('Unable to insert document.', err.stack)
+        }
+    
+        client.close();
+    }),
+
+
+    countTodos: (async (mongoUrl, dbName, collectionName, queryConstraint) => {
+        let client;
+    
+        try {
+            client = await MongoClient.connect(mongoUrl);
+            console.log("Connected correctly to server");
+    
+            const db = client.db(dbName);
+    
+            let r = await db.collection(collectionName).find().count();
+            
+            console.log(`Todos count: ${r}`);
+        } catch (err) {
+            console.log('Unable to insert document.', err.stack)
+        }
+    
+        client.close();
+    })
+
+}
